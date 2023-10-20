0604fabb36de8a13eb7ef5f0f0151fdef9cfabf1
Madrugamatos <mauricio_matoss@outlook.com>
2023-10-20 13:19:08 -0300

    Update jogo.py
    ajustes de problemas no jogo

--- jogodaforca/principal/jogo.py
+++ jogodaforca/principal/jogo.py
@@ -1,5 +1,5 @@
 #pasta = "funcao\dados2.txt"
-pasta1 = "principal\dados3.txt"
+pasta1 = "dados3.txt"
                                     
 cor = ('\033[m',      # 0 - sem cores
        '\033[0;31m',  # 1 - vermelho
@@ -12,21 +12,7 @@ cor = ('\033[m',      # 0 - sem cores
 
     )
 
-def aplicarCores(frase = " ",tom = 0):
-    cor = ('\033[m',         # 0 - sem cores
-       '\033[0;31m',  # 1 - vermelho
-       '\033[0;32m',  # 2 - verde
-       '\033[0;33m',  # 3 - amarelo
-       '\033[0;34m',  # 4 - azul
-       '\033[0;35m',  # 5 - roxo
-       '\033[7;30m' ,     # 6 - branco
-       '\033[0;30;42m',  # 7 - verde-fundo 
-       '\033[0;30;41m',  # 8 - vermelho-fundo
-       '\033[0;30;44m',  # 9 - azul-fundo
-       '\033[0;30;43m',  # 10 - amarelo-fundo
-    )
-    print(f"{cor[tom]}{frase}{cor[0]}")
-
+#essa função foi criada para formatar os nomes e pode tirar acentos espaços e copiar para um novo arquivo
 def copiararq (arquivo,leitor): 
     """
     #parametro arquivo: representa o arquivo limpo a ser aberto
@@ -54,6 +40,7 @@ def enfeite(msg='',enfe='*'):
     print(f'{msg:^30}')
     print(enfe*30)
 
+
 #programa principal
 from random import randint
 from time import sleep
@@ -66,167 +53,178 @@ except:
 else:
     print(f'Arquivo {pasta1} Aberto com sucesso.')
 
-acertos = cont = vof = v = 0
+acertos = 0
+cont = 0
 erros = []
 confirmados = []
 escolha = []
-
-
+vof = 0
+v = 0
 enfeite('START!')
 sleep(1)
-
-while True:
-    random =randint(0,407)
-    novo =dados[random].upper().replace(" ","-")
-    nome = novo.replace('\n','')
-
-    
-    enfeite('DICAS: ','-')
-    sleep(0.7)
-    if random < 201:
-        enfeite(f'Nome de Pessoa','-' )
-        sleep(0.7)
-    else:
-        enfeite(f'Nome de Animal','-') 
-        sleep(0.7)
-    enfeite(f"A Palavra tem {len(nome)} letras.",'-')
-    sleep(2)
-    #esse loop for é para preencher a lista com espaços da palavra sorteada.
-    for i in range(0,len(nome)):
-        escolha.append("_")
-    #esta condição é para palavras que tem o traço '-'
-
-    if '-' in nome:
-        for c,i in enumerate(nome):
-            if "-" == i:
-                del escolha[c]
-                escolha.insert(c,'-')
-                acertos+=1
-    #esse loop é para definir a quantidade de jogadas.           
+try:
     while True:
-       
-        while True:
-                    
-                letra = str(input(f"\n{cor[4]}Digite uma letra:{cor[0]} ")).upper()
-                if len(letra) > 1:
-                    aplicarCores('Digite apenas uma letra.',1)
-                elif str.isnumeric(letra) or not str.isalpha(letra):
-                    aplicarCores('Você não digitou uma letra.',1)                
-                else:
-                    break          
-            
-               
-
-
-        #essa condição é pra verificar se a letra digitada esta no nome selecionado
-        if letra not in escolha:
-            if letra in nome:       
-                for c,i in enumerate(nome):
-                    #essa condição é para adicionar a letra na posição correta na lista
-                    if letra == i:             
-                        del escolha[c]
-                        escolha.insert(c,letra)
-                        acertos +=1
-
-                # defini quando o jogador ganha, completando todas as letras.
-                if acertos >= len(nome) :
-                    aplicarCores('PARABENS VOCÊ GANHOU!!',2)                    
-                    aplicarCores(f'O nome era: ',3)
-                    aplicarCores(f"  {nome}",4)
-                    print(f"Você tentou {cont}",  "vez"if cont < 2 else f"vezes.")
-                    break
-
-            #adiciona as letra erradas a uma lista    
-            else:
-                if letra not in erros:
-                    erros.append(letra)
-                else:
-                    aplicarCores('Letra ja digitada.',1)
-                    cont-= 1 
-        else:
-            aplicarCores('Letra ja digitada.',1)
-            cont-= 1
-
-        #escreve a lista com as letra escolhidas e corretas.
-        sleep(1)
-        print('-'*len(nome))
-        aplicarCores('Acertos: \n',7)
-        for i in range(0,len(nome)):
-            print(f'{escolha[i]}', end=' ')
-        print('\n')
-        print('-'*len(nome))
-        sleep(0.5)
-
-        #print da lista de erros
-        aplicarCores('Erros:',8)
-        enfeite(f'{sorted(erros)}','*') 
-
-#opção de palpite, quando chegar a 50% de acertos ou proximo. 
-        if acertos >= int(len(nome)/2):
-            sleep(0.5)
-            while  vof < 1:
-                aplicarCores('Você quer arriscar o nome?',9)
-                digitar = str(input("[S/N]")).strip().upper()
-                if digitar == "S":
-                    while True:
-                        confirma = str(input("Digite seu palpite: ")).strip().upper()
-                        aplicarCores(f"Seu palpite foi: {confirma}",3)  
-                        aplicarCores('Esta Correto? ',9)                      
-                        con = str(input('[S/N]')).strip().upper()
-                        if con in "S":
-                            print('Verificando...')
-                            sleep(1)
-                            if confirma.replace(" ","-") == nome:
-
-                                print(f'{cor[2]}-{cor[0]}'*30)
-                                aplicarCores('   Parabens você ganhou!!',2) 
-                                print(f'{cor[2]}-{cor[0]}'*30)
-                                v += 1 
-                                vof += 1                              
-                                break    
-                            else: 
-                                print('Verificando...')
-                                sleep(1)                                                               
-                                aplicarCores('Você errou.',1)
-                                vof += 1
-                                break
-                                
-                        elif con not in "N":
-                            print("Digite S/N")
-                                
-                elif digitar == "N":
-                    vof+=1
-                else:
-                    print('Digite apenas S/N')
-            vof-=1 
-                                   
-        cont+=1
-        #frase avisando quantas jogadas falta.
-        print('-'*30)
-        print(f'Você tentou {cont}', "vez"if cont < 2 else "vezes",end=' ') 
-        print(f'ainda falta {10-cont}'if v < 1  else'.')
-        print('-'*30)
-        if v == 1:
+        try:
+            random =randint(0,407)
+            novo =dados[random].upper().replace(" ","-")
+            nome = novo.replace('\n','')
+        except:
+            print("""Verifique o caminho do arquivo "dados" na linha 2.\nVeja se o arquivo encontrase dentro da\nmesma pasta e se o caminho esta correto.
+                  """)
             break
-        elif cont == 10:
-            print(f'{cor[1]}-{cor[0]}'*30)
-            aplicarCores(f'       Você perdeu!!!',1)
-            print(f'{cor[1]}-{cor[0]}'*30)
-            aplicarCores(f'A palavra era {nome}',3)
-            break
+
         
-    #confirmação depois que acaba o jogo
-    aplicarCores("JOGAR MAIS UMA VEZ?",9)   
-    confir = str(input('S/N' )).strip().upper()
-    if confir == 'N':
-        break
-    # se for continuar zera a base de decisão do jogo
-    else:
-        escolha.clear()
-        erros.clear()
-        acertos = 0
-        cont = 0
-        v = 0
+        enfeite('DICAS: ','-')
+        sleep(0.7)
+        if random < 201:
+            enfeite(f'Nome de Pessoa','-' )
+            sleep(0.7)
+        else:
+            enfeite(f'Nome de Animal','-') 
+            sleep(0.7)
+        enfeite(f"A Palavra tem {len(nome)} letras.",'-')
+        sleep(2)
+        #esse loop for é para preencher a lista com espaços da palavra sorteada.
+        for i in range(0,len(nome)):
+            escolha.append("_")
+        #esta condição é para palavras que tem o traço '-'
 
+        if '-' in nome:
+            for c,i in enumerate(nome):
+                if "-" == i:
+                    del escolha[c]
+                    escolha.insert(c,'-')
+                    acertos+=1
+        #esse loop é para definir a quantidade de jogadas.           
+        while True:
+        
+            while True:
+                        
+                    letra = str(input(f"\nDigite uma letra:")).upper()
+                    if len(letra) > 1:
+                        print('Digite apenas uma letra.')
+                    elif str.isnumeric(letra) or not str.isalpha(letra):
+                       print('Você não digitou uma letra.')                
+                    else:
+                        break          
+                
+                
+
+
+            #essa condição é pra verificar se a letra digitada esta no nome selecionado
+            if letra not in escolha:
+                if letra in nome:       
+                    for c,i in enumerate(nome):
+                        #essa condição é para adicionar a letra na posição correta na lista
+                        if letra == i:             
+                            del escolha[c]
+                            escolha.insert(c,letra)
+                            acertos +=1
+
+                    # defini quando o jogador ganha, completando todas as letras.
+                    if acertos >= len(nome) :
+                        print('PARABENS VOCÊ GANHOU!!')                    
+                        print(f'O nome era: ')
+                        print(f"  {nome}")
+                        print(f"Você tentou {cont}",  "vez"if cont < 2 else f"vezes.")
+                        break
+
+                #adiciona as letra erradas a uma lista    
+                else:
+                    if letra not in erros:
+                        erros.append(letra)
+                    else:
+                        print('Letra ja digitada.')
+                        cont-= 1 
+            else:
+                print('Letra ja digitada.')
+                cont-= 1
+
+            #escreve a lista com as letra escolhidas e corretas.
+            sleep(1)
+            print('-'*len(nome))
+            print('Acertos: \n')
+            for i in range(0,len(nome)):
+                print(f'{escolha[i]}', end=' ')
+            print('\n')
+            print('-'*len(nome))
+            sleep(0.5)
+
+            #print da lista de erros
+            print('Erros:')
+            enfeite(f'{sorted(erros)}','*') 
+
+    #opção de palpite, quando chegar a 50% de acertos ou proximo. 
+            if acertos >= int(len(nome)/2):
+                sleep(0.5)
+                while  vof < 1:
+                    print('Você quer arriscar o nome?')
+                    digitar = str(input("[S/N]")).strip().upper()
+                    if digitar == "S":
+                        while True:
+                            confirma = str(input("Digite seu palpite: ")).strip().upper()
+                            print(f"Seu palpite foi: {confirma}")  
+                            print('Esta Correto? ')                      
+                            con = str(input('[S/N]')).strip().upper()
+                            if con in "S":
+                                
+                                if confirma.replace(" ","-") == nome:
+                                    print('Verificando...')
+                                    sleep(1)
+                                    print(f'-'*30)
+                                    print('   Parabens você ganhou!!') 
+                                    print(f'-'*30)
+                                    print(f'O nome era: ')
+                                    print(f"  {nome}")
+                                    v += 1 
+                                    vof += 1                              
+                                    break    
+                                else: 
+                                    print('Verificando...')
+                                    sleep(1)                                                               
+                                    print('Você errou.')
+                                    vof += 1
+                                    break
+                                    
+                            elif con not in "N":
+                                print("Digite S/N")
+                                    
+                    elif digitar == "N":
+                        vof+=1
+                    else:
+                        print('Digite apenas S/N')
+                vof-=1 
+                                    
+            cont+=1
+            #frase avisando quantas jogadas falta.
+            print('-'*30)
+            print(f'Você tentou {cont}', "vez"if cont < 2 else "vezes",end=' ') 
+            print(f'falta {10-cont}'if v < 1  else'.')
+            print('-'*30)
+            #finaliza o programa caso acerte o palpite
+            if v == 1:
+                break
+            elif cont == 10:
+                print(f'-'*30)
+                print(f'       Você perdeu!!!')
+                print(f'-'*30)
+                print(f'A palavra era {nome}')
+                break
+            
+        #confirmação depois que acaba o jogo
+        print("JOGAR MAIS UMA VEZ?")   
+        confir = str(input('S/N' )).strip().upper()
+        if confir == 'N':
+            break
+        # se for continuar zera a base de decisão do jogo
+        #v é usado em vitoria por palpite digitado.
+        else:
+            escolha.clear()
+            erros.clear()
+            acertos = 0
+            cont = 0
+            v = 0
+except KeyboardInterrupt:
+    print('Jogador saiu do jogo.')
 enfeite("FIM DO JOGO",'=')
   
